3e5d2bb4629ac5d508678dde32ee576214374900
Shehryar <shehryar@flicken.io>
2023-09-27 18:19:12 +0500

    Updated Code

--- books_scraper/books_scraper/spiders/my_spider.py
+++ books_scraper/books_scraper/spiders/my_spider.py
@@ -2,42 +2,35 @@ import scrapy
 from books_scraper.items import BooksScraperItem
 
 class MySpider(scrapy.Spider):
-    name = "xbot"
+    name = "Books_Details"
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
         category_links = response.css('div.side_categories > ul > li > ul > li > a')
         for genre in category_links:
             category_name = genre.css('::text').get().strip()
-            category_url = response.urljoin(genre.attrib['href'])
-            yield response.follow(category_url, callback=self.parse_category, meta={'category_name': category_name})
+            yield response.follow(genre.attrib['href'], callback=self.parse_category, meta={'category_name': category_name})
 
     def parse_category(self, response):
         category_name = response.meta['category_name']
         book_links = response.css('h3 > a::attr(href)').getall()
 
         for book_link in book_links:
-            book_url = book_link
-            yield response.follow(book_url, callback=self.parse_book, meta={'category_name': category_name})
+            yield response.follow(book_link, callback=self.parse_book, meta={'category_name': category_name})
 
+        next_page = response.css('li.next > a::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse_category, meta={'category_name': category_name})
     def parse_book(self, response):
-        category_name = response.meta['category_name']
-        book_name = response.css('div>h1::text').get()
-        book_description = response.css('meta[name="description"]::attr(content)').get().replace("...more", "")
-        book_upc = response.css('th:contains("UPC") + td::text').get()
-        book_price_excl_tax = response.css('th:contains("Price (excl. tax)") + td::text').get().replace("£", "")
-        book_price_incl_tax = response.css('th:contains("Price (incl. tax)") + td::text').get().replace("£", "")
-        book_availability = response.css('th:contains("Availability") + td::text').get()
-        book_reviews = response.css('th:contains("Number of reviews") + td::text').get()
 
         item = BooksScraperItem()
-        item['category'] = category_name
-        item['name'] = book_name.strip() if book_name else ""
-        item['description'] = book_description.strip() if book_description else ""
-        item['upc'] = book_upc.strip() if book_upc else ""
-        item['price_tax_exc'] = book_price_excl_tax.strip() if book_price_excl_tax else ""
-        item['price_tax_inc'] = book_price_incl_tax.strip() if book_price_incl_tax else ""
-        item['availability'] = book_availability.strip() if book_availability else ""
-        item['reviews'] = book_reviews.strip() if book_reviews else ""
+        item['category'] = response.meta['category_name']
+        item['name'] = response.css('div>h1::text').get("").strip()
+        item['description'] = response.css('meta[name="description"]::attr(content)').get("").replace("...more", "").strip()
+        item['upc'] = response.css('th:contains("UPC") + td::text').get("").strip()
+        item['price_tax_exc'] = response.css('th:contains("Price (excl. tax)") + td::text').get("").replace("£", "").strip()
+        item['price_tax_inc'] = response.css('th:contains("Price (incl. tax)") + td::text').get("").replace("£", "").strip()
+        item['availability'] = response.css('th:contains("Availability") + td::text').get("").strip()
+        item['reviews'] = response.css('th:contains("Number of reviews") + td::text').get("").strip()
 
         yield item
